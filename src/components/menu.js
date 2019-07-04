23da7a01967c15d6b91dfe3e9a56700bcfe54741
Matei Dorobantu <mdorobantu@docvue.com>
2019-07-04 15:54:22 -0600

    added icons via Fontawesome

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import {
+  Button,
   ButtonDropdown,
   DropdownToggle,
   DropdownMenu,
   DropdownItem
 } from 'reactstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSave } from '@fortawesome/free-solid-svg-icons'
 import allScenes from '../scenes/allScenes'
 import './menu.scss'
 
@@ -21,17 +24,23 @@ const Presets = props => (
   </>
 )
 
+const Saved = () => <DropdownItem disabled>{'- -'}</DropdownItem>
+
 const Menu = props => {
   const [isOpen, toggle] = useState(false)
   return (
     <div className="main-menu">
-      <ButtonDropdown isOpen={isOpen} toggle={() => toggle(!isOpen)}>
+      <Button className={'mx-1'}>
+        <FontAwesomeIcon icon={faSave} />
+      </Button>
+      <ButtonDropdown isOpen={isOpen} toggle={() => toggle(!isOpen)} className={'mx-1'}>
         <DropdownToggle caret>Select a Scene</DropdownToggle>
         <DropdownMenu>
           <DropdownItem header>Preset Scenes</DropdownItem>
           <Presets onClick={props.onClick} />
           <DropdownItem divider />
           <DropdownItem header>Your Saved Scenes</DropdownItem>
+          <Saved />
         </DropdownMenu>
       </ButtonDropdown>
     </div>
