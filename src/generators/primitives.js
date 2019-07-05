e9691d3cc1942c8ba814014b1294ac9d0bbc2801
Matei Dorobantu <mdorobantu@docvue.com>
2019-07-05 09:48:07 -0600

    added cap to random grays

--- src/generators/primitives.js
+++ src/generators/primitives.js
@@ -2,9 +2,9 @@ export const randomInt = (max = 255) => {
   return Math.floor(Math.random() * max)
 }
 
-export const randomGray = () => {
-  const g = randomInt()
-  return `rgb(${g},${g},${g})`
+export const randomGray = (cap = 255) => {
+  const component = randomInt(cap)
+  return `rgb(${component},${component},${component})`
 }
 
 export const randomColor = (cap = 255) => {
