07dea90bde7694d6f39931896443cb78eb3b4066
Matei Dorobantu <mateid@aedigital.com>
2020-11-21 16:57:43 -0700

    reduced to functional components using hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,21 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Canvas from './canvas'
 import Menu from './menu'
-import { UserContext, UserProvider } from '../contexts/userContext'
-import { getPreferences, setPreferences } from '../services/preferences'
 
-export default class App extends Component {
-  static contextType = UserContext
+const App = () => {
 
-  constructor(props) {
-    super(props)
+  const [scene, setScene] = useState({ renderer: () => { }});
+  const [preferences, setPreferences] = useState({});
 
-    this.state = {
-      renderFunction: () => {},
-      preferences: getPreferences()
-    }
-
-    this.handleClick = this.handleClick.bind(this)
-    this.handlePrefsClick = this.handlePrefsClick.bind(this)
-    this.handleSaveClick = this.handleSaveClick.bind(this)
-  }
-
-  handleClick(scene) {
-    this.setState({
-      ...this.state,
-      renderFunction: scene
-    })
-  }
-
-  handlePrefsClick() {
-    this.setState({
-      ...this.state,
-      preferences: { frame: !this.state.preferences.frame}
-    })
-    setPreferences(this.state.preferences)
-  }
-
-  handleSaveClick() {
-    console.log('Not implemented')
-  }
-
-  render() {
-    return (
-      <UserProvider value={this.state.preferences}>
-        <Menu
-          onClick={this.handleClick}
-          onPrefsClick={this.handlePrefsClick}
-          onSaveClick={this.handleSaveClick}
-        />
-        <Canvas render={this.state.renderFunction} />
-      </UserProvider>
-    )
-  }
+  return <>
+    <Menu
+      onClick={renderer => setScene({renderer})}
+      onPrefsClick={preferences => setPreferences(preferences)}
+      onSaveClick={() => console.warn('Not implemented!')}
+    />
+    <Canvas render={scene.renderer} />
+  </>
 }
+
+export default App;
--- src/components/canvas.js
+++ src/components/canvas.js
@@ -1,56 +1,52 @@
-import React, { Component } from 'react'
+import React, { useRef, useEffect } from 'react'
 import {UserConsumer} from '../contexts/userContext';
 import './canvas.scss'
 
-export default class Canvas extends Component {
-  constructor(props) {
-    super(props)
-    this.canvas = React.createRef()
-  }
+const Canvas = (props) => {
 
-  clear(ctx) {
+  const canvas = useRef()
+
+  useEffect(() => {
+    const dpr = window.devicePixelRatio
+    canvas.current.width = window.innerWidth * dpr
+    canvas.current.height = window.innerHeight * dpr
+    const ctx = canvas.current.getContext('2d')
+
+    ctx.scale(dpr, dpr)
+
+    renderScene()
+  })
+
+  const clear = ctx => {
     ctx.save()
     ctx.setTransform(1, 0, 0, 1, 0, 0)
-    ctx.clearRect(0, 0, this.canvas.current.width, this.canvas.current.height)
+    ctx.clearRect(0, 0, canvas.current.width, canvas.current.height)
     ctx.restore()
   }
 
-  renderScene() {
-    const ctx = this.canvas.current.getContext('2d')
-    this.clear(ctx)
+  const renderScene = () => {
+    const ctx = canvas.current.getContext('2d')
+    clear(ctx)
     setTimeout(
       () =>
-        this.props.render(
+        props.render(
           ctx,
-          this.canvas.current.width,
-          this.canvas.current.height
+          canvas.current.width,
+          canvas.current.height
         ),
       0
     )
   }
 
-  componentDidMount() {
-    const dpr = window.devicePixelRatio
-    this.canvas.current.width = window.innerWidth * dpr
-    this.canvas.current.height = window.innerHeight * dpr
-    const ctx = this.canvas.current.getContext('2d')
-
-    ctx.scale(dpr, dpr)
-  }
-
-  componentDidUpdate() {
-    this.renderScene()
-  }
-
-  render() {
-    return (
-      <UserConsumer>
-        {props => 
-            <div className={props.frame ? 'frame' : ''}>
-              <canvas ref={this.canvas} className="main-canvas" />
-            </div>
-      }
-      </UserConsumer>
-    )
-  }
+  return (
+    <UserConsumer>
+      {props => 
+          <div className={props.frame ? 'frame' : ''}>
+            <canvas ref={canvas} className="main-canvas" />
+          </div>
+    }
+    </UserConsumer>
+  )
 }
+
+export default Canvas
